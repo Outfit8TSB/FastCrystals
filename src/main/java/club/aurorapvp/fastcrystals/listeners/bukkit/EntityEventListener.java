f45b7a42eab3d5a90a407b5f85e1200d4ab930f6
Villagers654 <thbvill@gmail.com>
2023-09-30 13:13:13 +0900

    Adjust event priorities for more safety

--- src/main/java/club/aurorapvp/fastcrystals/listeners/bukkit/EntityEventListener.java
+++ src/main/java/club/aurorapvp/fastcrystals/listeners/bukkit/EntityEventListener.java
@@ -5,12 +5,14 @@ import com.destroystokyo.paper.event.entity.EntityRemoveFromWorldEvent;
 import org.bukkit.entity.EnderCrystal;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class EntityEventListener implements Listener {
   @EventHandler
+      (priority = EventPriority.MONITOR)
   public void onEntityRemoveFromWorld(EntityRemoveFromWorldEvent event) {
     if (event.getEntityType() != EntityType.ENDER_CRYSTAL) {
       return;
@@ -25,6 +27,7 @@ public class EntityEventListener implements Listener {
   }
 
   @EventHandler
+      (priority = EventPriority.MONITOR)
   public void onEntitySpawn(EntitySpawnEvent event) {
     if (event.getEntityType() != EntityType.ENDER_CRYSTAL) {
       return;
